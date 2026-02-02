930c12dddc28a251d49fdbb8104f32ca4eae25e6
sujaloraon757-sys <sujaloraon757@gmail.com>
2026-02-03 05:22:27 +0530

    lorem

---
+++ script.js
@@ -0,0 +1,8 @@
+console.log("This is JavaScript code.");
+let myVariable = 42;
+console.log("The value of myVariable is: " + myVariable);
+function greet(name) {
+    return "Hello, " + name + "!";
+}
+let name="Sujal";
+console.log(greet(name));
